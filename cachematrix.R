58ee7ea460cdf7c13e3d8b82a461def722a1eec1
miltonbahia <milton.bahia@gmail.com>
2015-04-25 09:28:22 -0300

    Final version  - Programming 2
    Final version of makeCacheMatrix and cacheSolve functions. Improved
    comments and code page structure

--- cachematrix.R
+++ cachematrix.R
@@ -4,56 +4,74 @@ makeCacheMatrix <- function(x = matrix()) {
         
     inv <- NULL
     
+    # set the new value of the matrix and clear the 
+    # value of the inverse matrix 
     
     set <- function(y) {
         x <<- y
         inv <<- NULL
     }
     
+    #  get the value of the matrix
+        
     get <- function() x
     
+    # set the value of the inverse matrix
+        
     setinverse <- function(inverse) inv <<- inverse
     
+    # get the value of the inverse matrix
+    
     getinverse <- function() inv
+        
+    # Return a "matrix" object
     
-    
-    list(set = set, get = get,
-         setinverse = setinverse,
-         getinverse = getinverse)
+    list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)
 }
 
 ## This function computes the inverse of the special "matrix" returned by 
-## makeCacheMatrix above. If the inverse has already been calculated 
-## (and the matrix has not changed), then the cachesolve should retrieve the 
-## inverse from the cache
+## makeCacheMatrix above. 
 
 cacheSolve <- function(x, ...) {
     
         
     inv <- x$getinverse()
 
+    # Checks whether the inverse of 'x' has been calculated previously 
+    # and is cached.
+    
     if(!is.null(inv)) {
+        
+        # Retrieves the cached value
+        
         message("getting cached data")
         return(inv)
-    }
-        
-    datamatrix <- x$get()
-    
-    ## Checks if 'x' is a square matrix
-    
-    if (nrow(datamatrix)==ncol(datamatrix)) {
-    
-        inv <- solve(datamatrix, ...)        
-        x$setinverse(inv)
-        
     }
     
     else {
     
-      stop("'x' is not a square matrix")
+        #  Computes the inverse matrix and caches  
         
+        datamatrix <- x$get()
+        
+        # Checks if 'x' is a square matrix and performs the computation 
+        # of the inverse
+                
+        if (nrow(datamatrix)==ncol(datamatrix)) {
+        
+            inv <- solve(datamatrix, ...)        
+            x$setinverse(inv)
+            
+        }
+        
+        else {
+        
+          stop("'x' is not a square matrix")
+            
+        }
+    
     }
-        
+    
     ## Return a matrix that is the inverse of 'x'
     
     inv
